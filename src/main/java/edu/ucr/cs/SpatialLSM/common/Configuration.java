93a3c1295517d94ba03c357cd3ec0ef003b69cfd
autopear <lonelystars@gmail.com>
2020-04-23 15:17:22 -0700

    Allow to set common default parameters.

--- src/main/java/edu/ucr/cs/SpatialLSM/common/Configuration.java
+++ src/main/java/edu/ucr/cs/SpatialLSM/common/Configuration.java
@@ -53,18 +53,74 @@ public class Configuration {
             } else {
                 privateIP = resolvePrivateIP(nodeName);
             }
-            feedPort = Integer.parseInt(jsonObject.get("feed_port").toString());
-            asterixDBPath = Utils.formatPath(jsonObject.get("asterixdb").toString());
-            startScript = Utils.formatPath(asterixDBPath + "/opt/local/bin/start-sample-cluster.sh");
-            stopScript = Utils.formatPath(asterixDBPath + "/opt/local/bin/stop-sample-cluster.sh");
-            resetDBPath = Utils.formatPath(jsonObject.get("reset_db").toString());
-            logParserPath = Utils.formatPath(jsonObject.get("log_parser").toString());
-            logsDir = Utils.formatPath(jsonObject.get("logs_dir").toString());
             taskName = jsonObject.get("task").toString().trim();
-            readLogPath = Utils.formatPath(logsDir + "/" + taskName + ".read.tsv");
             rtreePolicy = jsonObject.get("rtree").toString().replace("[\r\n]", " ").replace("\"", "\\\"");
             while (rtreePolicy.contains("  "))
                 rtreePolicy = rtreePolicy.replace("  ", " ");
+            if (jsonObject.containsKey("common")) {
+                String commonConfigPath = jsonObject.get("common").toString();
+                Reader commonReader = new FileReader(commonConfigPath);
+                JSONObject commonObject = (JSONObject) parser.parse(commonReader);
+                if (commonObject.containsKey("feed_port"))
+                    feedPort = Integer.parseInt(commonObject.get("feed_port").toString());
+                if (commonObject.containsKey("asterixdb"))
+                    asterixDBPath = Utils.formatPath(commonObject.get("asterixdb").toString());
+                if (commonObject.containsKey("reset_db"))
+                    resetDBPath = Utils.formatPath(commonObject.get("reset_db").toString());
+                if (commonObject.containsKey("log_parser"))
+                    logParserPath = Utils.formatPath(commonObject.get("log_parser").toString());
+                if (commonObject.containsKey("logs_dir"))
+                    logsDir = Utils.formatPath(commonObject.get("logs_dir").toString());
+                if (commonObject.containsKey("duration"))
+                    duration = Long.parseLong(commonObject.get("duration").toString()) * 1000;
+                if (commonObject.containsKey("size_load"))
+                    sizeLoad = Long.parseLong(commonObject.get("size_load").toString());
+                if (commonObject.containsKey("batch_insert"))
+                    batchSizeInsert = Long.parseLong(commonObject.get("batch_insert").toString());
+                if (commonObject.containsKey("batch_read"))
+                    batchSizeRead = Long.parseLong(commonObject.get("batch_read").toString());
+                if (commonObject.containsKey("num_insert"))
+                    numBatchInsert = Integer.parseInt(commonObject.get("num_insert").toString());
+                if (commonObject.containsKey("num_read"))
+                    numBatchRead = Integer.parseInt(commonObject.get("num_read").toString());
+                if (commonObject.containsKey("threads_load"))
+                    numThreadsLoad = Integer.parseInt(commonObject.get("threads_load").toString());
+                if (commonObject.containsKey("threads_insert"))
+                    numThreadsInsert = Integer.parseInt(commonObject.get("threads_insert").toString());
+                if (commonObject.containsKey("threads_read"))
+                    numThreadsRead = Integer.parseInt(commonObject.get("threads_read").toString());
+                if (commonObject.containsKey("space")) {
+                    JSONArray space = (JSONArray) commonObject.get("space");
+                    spaceWidth = Double.parseDouble(space.get(0).toString());
+                    spaceHeight = Double.parseDouble(space.get(1).toString());
+                }
+                if (commonObject.containsKey("scales")) {
+                    JSONArray arr = (JSONArray) commonObject.get("scales");
+                    scales = new double[arr.size()];
+                    for (int i = 0; i < arr.size(); i++)
+                        scales[i] = str2d(arr.get(i).toString());
+                }
+                if (commonObject.containsKey("sleep_load"))
+                    loadSleep = Long.parseLong(commonObject.get("sleep_load").toString());
+                if (commonObject.containsKey("sleep_insert"))
+                    insertSleep = Long.parseLong(commonObject.get("sleep_insert").toString());
+                if (commonObject.containsKey("sleep_read"))
+                    readSleep = Long.parseLong(commonObject.get("sleep_read").toString());
+                commonReader.close();
+            }
+            if (jsonObject.containsKey("feed_port"))
+                feedPort = Integer.parseInt(jsonObject.get("feed_port").toString());
+            if (jsonObject.containsKey("asterixdb"))
+                asterixDBPath = Utils.formatPath(jsonObject.get("asterixdb").toString());
+            startScript = Utils.formatPath(asterixDBPath + "/opt/local/bin/start-sample-cluster.sh");
+            stopScript = Utils.formatPath(asterixDBPath + "/opt/local/bin/stop-sample-cluster.sh");
+            if (jsonObject.containsKey("reset_db"))
+                resetDBPath = Utils.formatPath(jsonObject.get("reset_db").toString());
+            if (jsonObject.containsKey("log_parser"))
+                logParserPath = Utils.formatPath(jsonObject.get("log_parser").toString());
+            if (jsonObject.containsKey("logs_dir"))
+                logsDir = Utils.formatPath(jsonObject.get("logs_dir").toString());
+            readLogPath = Utils.formatPath(logsDir + "/" + taskName + ".read.tsv");
             if (jsonObject.containsKey("duration"))
                 duration = Long.parseLong(jsonObject.get("duration").toString()) * 1000;
             if (jsonObject.containsKey("size_load"))
@@ -89,10 +145,10 @@ public class Configuration {
                 spaceHeight = Double.parseDouble(space.get(1).toString());
             }
             if (jsonObject.containsKey("scales")) {
-                JSONArray arr = (JSONArray) jsonObject.get("exps");
+                JSONArray arr = (JSONArray) jsonObject.get("scales");
                 scales = new double[arr.size()];
                 for (int i = 0; i < arr.size(); i++)
-                    scales[i] = Double.parseDouble(arr.get(i).toString());
+                    scales[i] = str2d(arr.get(i).toString());
             }
             if (jsonObject.containsKey("sleep_load"))
                 loadSleep = Long.parseLong(jsonObject.get("sleep_load").toString());
@@ -100,6 +156,7 @@ public class Configuration {
                 insertSleep = Long.parseLong(jsonObject.get("sleep_insert").toString());
             if (jsonObject.containsKey("sleep_read"))
                 readSleep = Long.parseLong(jsonObject.get("sleep_read").toString());
+            configReader.close();
             configIsValid = true;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
@@ -110,6 +167,20 @@ public class Configuration {
     private String resolvePrivateIP(final String host) {
         String output = Utils.getCommandOutput("ssh " + host + " \"hostname -I | awk '{{print $1}}'\"");
         return output.replaceAll("[\r\n]]", "").trim();
+    }
+
+    private static double str2d(String s) {
+        String ss = s.toLowerCase();
+        if (ss.contains("e"))
+            return Double.parseDouble(ss);
+        else {
+            String[] ds = ss.split("e");
+            if (ds.length != 2)
+                throw new NumberFormatException(s + " cannot be parsed to double type.");
+            double b = Double.parseDouble(ds[0]);
+            double e = Double.parseDouble(ds[1].startsWith("+") ? ds[1].substring(1) : ds[1]);
+            return Math.pow(b, e);
+        }
     }
 
     public boolean isValid() {
